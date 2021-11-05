5de59e14fabc1db04e6389747042cf262aff4237
Luis Luna <siul-luna@hotmail.com>
2021-11-05 15:30:03 -0600

    Boton Trasladar

--- StockTransfer/CatchingEvents.vb
+++ StockTransfer/CatchingEvents.vb
@@ -1,9 +1,9 @@
 ﻿Public Class CatchingEvents
 
+
     Friend WithEvents SBOApplication As SAPbouiCOM.Application '//OBJETO DE APLICACION
     Friend SBOCompany As SAPbobsCOM.Company '//OBJETO COMPAÑIA
     Friend csDirectory As String '//DIRECTORIO DONDE SE ENCUENTRAN LOS .SRF
-    Dim DocNum As String
 
 
     Public Sub New()
@@ -77,6 +77,8 @@
 
         Try
             lofilters = New SAPbouiCOM.EventFilters
+            lofilter = lofilters.Add(SAPbouiCOM.BoEventTypes.et_FORM_LOAD)
+            lofilter.AddEx(1250000940) '// FORMA Solicitud de traslado
             lofilter = lofilters.Add(SAPbouiCOM.BoEventTypes.et_ITEM_PRESSED)
             lofilter.AddEx(1250000940) '// FORMA Solicitud de traslado
 
@@ -110,14 +112,8 @@
 
         If pVal.Before_Action = True And pVal.FormTypeEx <> "" Then
 
-            Select Case pVal.FormTypeEx
-
-                Case 1250000940                           '////// FORMA Solicitud de traslado
-                    frmOWTRControllerBefore(FormUID, pVal)
-
-            End Select
-
         Else
+
             If pVal.Before_Action = False And pVal.FormTypeEx <> "" Then
                 Select Case pVal.FormTypeEx
 
@@ -126,42 +122,8 @@
 
                 End Select
             End If
+
         End If
-
-    End Sub
-
-
-    Private Sub frmOWTRControllerBefore(ByVal FormUID As String, ByVal pVal As SAPbouiCOM.ItemEvent)
-
-        Dim coForm As SAPbouiCOM.Form
-        Dim stTabla As String
-        Dim oDatatable As SAPbouiCOM.DBDataSource
-
-        Try
-
-            Select Case pVal.EventType
-
-                Case SAPbouiCOM.BoEventTypes.et_ITEM_PRESSED
-
-                    Select Case pVal.ItemUID
-
-                        Case 1
-
-                            stTabla = "OWTQ"
-                            coForm = SBOApplication.Forms.Item(FormUID)
-
-                            oDatatable = coForm.DataSources.DBDataSources.Item(stTabla)
-                            DocNum = oDatatable.GetValue("DocNum", 0)
-
-                    End Select
-
-            End Select
-
-        Catch ex As Exception
-            SBOApplication.MessageBox("Error en el evento sobre Forma Facturacion Clientes. " & ex.Message)
-        Finally
-            'oPO = Nothing
-        End Try
 
     End Sub
 
@@ -170,141 +132,43 @@
     '// CONTROLADOR DE EVENTOS FORMA Solicitud de traslado
     '//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     Private Sub frmOWTRControllerAfter(ByVal FormUID As String, ByVal pVal As SAPbouiCOM.ItemEvent)
-        Dim DocEntry, ObjType, LineNum, ItemCode, FromWhsCod, WhsCode, BatchNumber, DocNumST As String
-        Dim Quantity As Double
-        Dim stQueryH1, stQueryH2, stQueryH3 As String
-        Dim oRecSetH1, oRecSetH2, oRecSetH3 As SAPbobsCOM.Recordset
-        Dim oStockTransfer As SAPbobsCOM.StockTransfer
-        Dim CantidadR, CantidadL As Double
-        Dim llError As Long
-        Dim lsError As String
-        Dim AOWTR As Integer
-        Dim oED As FrmtekEDocument
 
-        oRecSetH1 = SBOCompany.GetBusinessObject(SAPbobsCOM.BoObjectTypes.BoRecordset)
-        oRecSetH2 = SBOCompany.GetBusinessObject(SAPbobsCOM.BoObjectTypes.BoRecordset)
-        oRecSetH3 = SBOCompany.GetBusinessObject(SAPbobsCOM.BoObjectTypes.BoRecordset)
-
-        oStockTransfer = SBOCompany.GetBusinessObject(SAPbobsCOM.BoObjectTypes.oStockTransfer)
+        Dim obotton As OWTQ
+        Dim oOWTR As Trasladar
+        Dim coForm As SAPbouiCOM.Form
+        Dim stTabla As String
+        Dim oDatatable As SAPbouiCOM.DBDataSource
+        Dim DocNum As String
+        Dim Respuesta As Integer
 
         Try
 
             Select Case pVal.EventType
+
+                 '///// Carga de formas
+                Case SAPbouiCOM.BoEventTypes.et_FORM_LOAD
+                    obotton = New OWTQ
+                    obotton.addFormItems(FormUID)
 
                 '//////Evento Presionar Item
                 Case SAPbouiCOM.BoEventTypes.et_ITEM_PRESSED
 
                     Select Case pVal.ItemUID
 
-                        Case 1
+                        Case "btTrnsfr"
 
-                            stQueryH1 = "Select T1.""DocEntry"",T0.""ObjType"",T1.""LineNum"",T1.""ItemCode"",T1.""FromWhsCod"",T1.""WhsCode"",T1.""Quantity"" from OWTQ T0 Inner Join WTQ1 T1 on T1.""DocEntry""=T0.""DocEntry"" where T0.""DocNum""=" & DocNum
-                            oRecSetH1.DoQuery(stQueryH1)
+                            Respuesta = SBOApplication.MessageBox("¿Deseas crear el Traslado?", Btn1Caption:="Si", Btn2Caption:="No")
 
-                            If oRecSetH1.RecordCount > 0 Then
+                            If Respuesta = 1 Then
 
-                                oStockTransfer.DocDate = DateTime.Now
-                                oStockTransfer.ElectronicProtocols.GenerationType = 1
-                                oStockTransfer.ElectronicProtocols.Add()
+                                stTabla = "OWTQ"
+                                coForm = SBOApplication.Forms.Item(FormUID)
 
-                                oRecSetH1.MoveFirst()
+                                oDatatable = coForm.DataSources.DBDataSources.Item(stTabla)
+                                DocNum = oDatatable.GetValue("DocNum", 0)
 
-                                For i = 0 To oRecSetH1.RecordCount - 1
-
-                                    DocEntry = oRecSetH1.Fields.Item("DocEntry").Value
-                                    ObjType = oRecSetH1.Fields.Item("ObjType").Value
-                                    LineNum = oRecSetH1.Fields.Item("LineNum").Value
-                                    ItemCode = oRecSetH1.Fields.Item("ItemCode").Value
-                                    FromWhsCod = oRecSetH1.Fields.Item("FromWhsCod").Value
-                                    WhsCode = oRecSetH1.Fields.Item("WhsCode").Value
-                                    Quantity = oRecSetH1.Fields.Item("Quantity").Value
-
-                                    oStockTransfer.Lines.BaseEntry = DocEntry
-                                    oStockTransfer.Lines.BaseType = 5
-                                    oStockTransfer.Lines.BaseLine = LineNum
-                                    oStockTransfer.Lines.ItemCode = ItemCode
-                                    oStockTransfer.Lines.FromWarehouseCode = FromWhsCod
-                                    oStockTransfer.Lines.WarehouseCode = WhsCode
-                                    oStockTransfer.Lines.Quantity = Quantity
-
-                                    stQueryH2 = "Select T0.*,T1.""CreateDate"" from
-                                                    (Select ""BatchNum"",""ItemCode"",""WhsCode"",
-                                                    sum(case when ""Direction""=0 then ""Quantity"" else -1*""Quantity"" end) as ""CantidadLote"" 
-                                                    from IBT1 where ""ItemCode""='" & ItemCode & "' AND ""WhsCode""='" & FromWhsCod & "'
-                                                    Group by  ""BatchNum"",""ItemCode"",""WhsCode"") T0
-                                                    Inner Join OBTN T1 on T1.""DistNumber""=T0.""BatchNum"" and T1.""ItemCode""=T0.""ItemCode""
-                                                    where T0.""CantidadLote"">0
-                                                    order by T1.""CreateDate"""
-                                    oRecSetH2.DoQuery(stQueryH2)
-
-                                    If oRecSetH2.RecordCount > 0 Then
-
-                                        oRecSetH2.MoveFirst()
-                                        CantidadR = Quantity
-
-                                        For l = 0 To oRecSetH2.RecordCount - 1
-
-                                            CantidadL = oRecSetH2.Fields.Item("CantidadLote").Value
-
-                                            If CantidadR > CantidadL Then
-
-                                                CantidadR = CantidadR - CantidadL
-
-                                                BatchNumber = oRecSetH2.Fields.Item("BatchNum").Value
-
-                                                oStockTransfer.Lines.BatchNumbers.BatchNumber = BatchNumber
-                                                oStockTransfer.Lines.BatchNumbers.Quantity = CantidadL
-                                                oStockTransfer.Lines.BatchNumbers.BaseLineNumber = LineNum
-
-                                                oStockTransfer.Lines.BatchNumbers.Add()
-
-                                                l = 0
-
-                                            Else
-
-                                                BatchNumber = oRecSetH2.Fields.Item("BatchNum").Value
-
-                                                oStockTransfer.Lines.BatchNumbers.BatchNumber = BatchNumber
-                                                oStockTransfer.Lines.BatchNumbers.Quantity = CantidadR
-                                                oStockTransfer.Lines.BatchNumbers.BaseLineNumber = LineNum
-
-                                                oStockTransfer.Lines.BatchNumbers.Add()
-
-                                                l = oRecSetH2.RecordCount - 1
-
-                                            End If
-
-                                            oRecSetH2.MoveNext()
-
-                                        Next
-
-                                    End If
-
-                                    oStockTransfer.Lines.Add()
-                                    oRecSetH1.MoveNext()
-
-                                Next
-
-                                If oStockTransfer.Add() <> 0 Then
-
-                                    SBOCompany.GetLastError(llError, lsError)
-                                    Err.Raise(-1, 1, lsError)
-
-                                Else
-
-                                    AOWTR = SBOCompany.GetNewObjectKey().ToString()
-                                    stQueryH3 = "Select ""DocNum"" from OWTR where ""DocEntry""=" & AOWTR
-                                    oRecSetH3.DoQuery(stQueryH3)
-
-                                    If oRecSetH3.RecordCount = 1 Then
-
-                                        DocNumST = oRecSetH3.Fields.Item("DocNum").Value
-                                        oED = New FrmtekEDocument
-                                        oED.openForm(csDirectory, AOWTR)
-
-                                    End If
-
-                                End If
+                                oOWTR = New Trasladar
+                                oOWTR.AddTransfer(csDirectory, DocNum)
 
                             End If
 
@@ -313,10 +177,13 @@
             End Select
 
         Catch ex As Exception
+
             SBOApplication.MessageBox("Error en el evento sobre Forma Solicitud de traslado. " & ex.Message)
+
         Finally
             'oPO = Nothing
         End Try
+
     End Sub
 
 
---
+++ StockTransfer/Trasladar.vb
@@ -0,0 +1,162 @@
+﻿Public Class Trasladar
+
+
+    Private cSBOApplication As SAPbouiCOM.Application '//OBJETO DE APLICACION
+    Private cSBOCompany As SAPbobsCOM.Company     '//OBJETO DE CONEXION
+    Private coForm As SAPbouiCOM.Form           '//FORMA
+    Private csFormUID As String
+    Private stDocNum As String
+    Friend Monto As Double
+    Dim ContOBNK, AORIN As Integer
+
+
+    '//----- METODO DE CREACION DE LA CLASE
+    Public Sub New()
+        MyBase.New()
+        cSBOApplication = oCatchingEvents.SBOApplication
+        cSBOCompany = oCatchingEvents.SBOCompany
+        Me.stDocNum = stDocNum
+    End Sub
+
+
+    Public Function AddTransfer(ByVal csDirectory As String, ByVal DocNum As String)
+
+        Dim DocEntry, ObjType, LineNum, ItemCode, FromWhsCod, WhsCode, BatchNumber, DocNumST As String
+        Dim Quantity As Double
+        Dim stQueryH1, stQueryH2, stQueryH3 As String
+        Dim oRecSetH1, oRecSetH2, oRecSetH3 As SAPbobsCOM.Recordset
+        Dim oStockTransfer As SAPbobsCOM.StockTransfer
+        Dim CantidadR, CantidadL As Double
+        Dim llError As Long
+        Dim lsError As String
+        Dim AOWTR As Integer
+        Dim oED As FrmtekEDocument
+
+        oRecSetH1 = cSBOCompany.GetBusinessObject(SAPbobsCOM.BoObjectTypes.BoRecordset)
+        oRecSetH2 = cSBOCompany.GetBusinessObject(SAPbobsCOM.BoObjectTypes.BoRecordset)
+        oRecSetH3 = cSBOCompany.GetBusinessObject(SAPbobsCOM.BoObjectTypes.BoRecordset)
+
+        oStockTransfer = cSBOCompany.GetBusinessObject(SAPbobsCOM.BoObjectTypes.oStockTransfer)
+
+        Try
+
+            stQueryH1 = "Select T1.""DocEntry"",T0.""ObjType"",T1.""LineNum"",T1.""ItemCode"",T1.""FromWhsCod"",T1.""WhsCode"",T1.""Quantity"" from OWTQ T0 Inner Join WTQ1 T1 on T1.""DocEntry""=T0.""DocEntry"" where T0.""DocNum""=" & DocNum
+            oRecSetH1.DoQuery(stQueryH1)
+
+            If oRecSetH1.RecordCount > 0 Then
+
+                oStockTransfer.DocDate = DateTime.Now
+                oStockTransfer.ElectronicProtocols.GenerationType = 1
+                oStockTransfer.ElectronicProtocols.Add()
+
+                oRecSetH1.MoveFirst()
+
+                For i = 0 To oRecSetH1.RecordCount - 1
+
+                    DocEntry = oRecSetH1.Fields.Item("DocEntry").Value
+                    ObjType = oRecSetH1.Fields.Item("ObjType").Value
+                    LineNum = oRecSetH1.Fields.Item("LineNum").Value
+                    ItemCode = oRecSetH1.Fields.Item("ItemCode").Value
+                    FromWhsCod = oRecSetH1.Fields.Item("FromWhsCod").Value
+                    WhsCode = oRecSetH1.Fields.Item("WhsCode").Value
+                    Quantity = oRecSetH1.Fields.Item("Quantity").Value
+
+                    oStockTransfer.Lines.BaseEntry = DocEntry
+                    oStockTransfer.Lines.BaseType = 5
+                    oStockTransfer.Lines.BaseLine = LineNum
+                    oStockTransfer.Lines.ItemCode = ItemCode
+                    oStockTransfer.Lines.FromWarehouseCode = FromWhsCod
+                    oStockTransfer.Lines.WarehouseCode = WhsCode
+                    oStockTransfer.Lines.Quantity = Quantity
+
+                    stQueryH2 = "Select T0.*,T1.""CreateDate"" from
+                                                    (Select ""BatchNum"",""ItemCode"",""WhsCode"",
+                                                    sum(case when ""Direction""=0 then ""Quantity"" else -1*""Quantity"" end) as ""CantidadLote"" 
+                                                    from IBT1 where ""ItemCode""='" & ItemCode & "' AND ""WhsCode""='" & FromWhsCod & "'
+                                                    Group by  ""BatchNum"",""ItemCode"",""WhsCode"") T0
+                                                    Inner Join OBTN T1 on T1.""DistNumber""=T0.""BatchNum"" and T1.""ItemCode""=T0.""ItemCode""
+                                                    where T0.""CantidadLote"">0
+                                                    order by T1.""CreateDate"""
+                    oRecSetH2.DoQuery(stQueryH2)
+
+                    If oRecSetH2.RecordCount > 0 Then
+
+                        oRecSetH2.MoveFirst()
+                        CantidadR = Quantity
+
+                        For l = 0 To oRecSetH2.RecordCount - 1
+
+                            CantidadL = oRecSetH2.Fields.Item("CantidadLote").Value
+
+                            If CantidadR > CantidadL Then
+
+                                CantidadR = CantidadR - CantidadL
+
+                                BatchNumber = oRecSetH2.Fields.Item("BatchNum").Value
+
+                                oStockTransfer.Lines.BatchNumbers.BatchNumber = BatchNumber
+                                oStockTransfer.Lines.BatchNumbers.Quantity = CantidadL
+                                oStockTransfer.Lines.BatchNumbers.BaseLineNumber = LineNum
+
+                                oStockTransfer.Lines.BatchNumbers.Add()
+
+                                l = 0
+
+                            Else
+
+                                BatchNumber = oRecSetH2.Fields.Item("BatchNum").Value
+
+                                oStockTransfer.Lines.BatchNumbers.BatchNumber = BatchNumber
+                                oStockTransfer.Lines.BatchNumbers.Quantity = CantidadR
+                                oStockTransfer.Lines.BatchNumbers.BaseLineNumber = LineNum
+
+                                oStockTransfer.Lines.BatchNumbers.Add()
+
+                                l = oRecSetH2.RecordCount - 1
+
+                            End If
+
+                            oRecSetH2.MoveNext()
+
+                        Next
+
+                    End If
+
+                    oStockTransfer.Lines.Add()
+                    oRecSetH1.MoveNext()
+
+                Next
+
+                If oStockTransfer.Add() <> 0 Then
+
+                    cSBOCompany.GetLastError(llError, lsError)
+                    Err.Raise(-1, 1, lsError)
+
+                Else
+
+                    AOWTR = cSBOCompany.GetNewObjectKey().ToString()
+                    stQueryH3 = "Select ""DocNum"" from OWTR where ""DocEntry""=" & AOWTR
+                    oRecSetH3.DoQuery(stQueryH3)
+
+                    If oRecSetH3.RecordCount = 1 Then
+
+                        DocNumST = oRecSetH3.Fields.Item("DocNum").Value
+                        oED = New FrmtekEDocument
+                        oED.openForm(csDirectory, AOWTR)
+
+                    End If
+
+                End If
+
+            End If
+
+        Catch ex As Exception
+
+            cSBOApplication.MessageBox("Error al crear el traslado. " & ex.Message)
+
+        End Try
+
+    End Function
+
+
+End Class
