5bfc704c647dfc23c28ffe776ad74510e60815f7
Luis Luna <siul-luna@hotmail.com>
2021-11-10 13:37:06 -0600

    Mejora Prformance

--- StockTransfer/Trasladar.vb
+++ StockTransfer/Trasladar.vb
@@ -21,7 +21,7 @@
 
     Public Function AddTransfer(ByVal csDirectory As String, ByVal DocNum As String)
 
-        Dim DocEntry, ObjType, LineNum, ItemCode, FromWhsCod, WhsCode, BatchNumber, DocNumST As String
+        Dim DocEntry, ObjType, LineNum, ItemCode, VisOrder, FromWhsCod, WhsCode, BatchNumber, DocNumST, Lote As String
         Dim Quantity As Double
         Dim stQueryH1, stQueryH2, stQueryH3 As String
         Dim oRecSetH1, oRecSetH2, oRecSetH3 As SAPbobsCOM.Recordset
@@ -40,7 +40,7 @@
 
         Try
 
-            stQueryH1 = "Select T1.""DocEntry"",T0.""ObjType"",T1.""LineNum"",T1.""ItemCode"",T1.""FromWhsCod"",T1.""WhsCode"",T1.""Quantity"" from OWTQ T0 Inner Join WTQ1 T1 on T1.""DocEntry""=T0.""DocEntry"" where T0.""DocNum""=" & DocNum
+            stQueryH1 = "Select T1.""DocEntry"",T0.""ObjType"",T1.""LineNum"",T1.""ItemCode"",T1.""VisOrder"",T1.""FromWhsCod"",T1.""WhsCode"",T1.""Quantity"",T2.""ManBtchNum"" from OWTQ T0 Inner Join WTQ1 T1 on T1.""DocEntry""=T0.""DocEntry"" Inner Join OITM T2 on T2.""ItemCode""=T1.""ItemCode"" where T0.""DocNum""=" & DocNum
             oRecSetH1.DoQuery(stQueryH1)
 
             If oRecSetH1.RecordCount > 0 Then
@@ -56,10 +56,12 @@
                     DocEntry = oRecSetH1.Fields.Item("DocEntry").Value
                     ObjType = oRecSetH1.Fields.Item("ObjType").Value
                     LineNum = oRecSetH1.Fields.Item("LineNum").Value
+                    VisOrder = oRecSetH1.Fields.Item("VisOrder").Value
                     ItemCode = oRecSetH1.Fields.Item("ItemCode").Value
                     FromWhsCod = oRecSetH1.Fields.Item("FromWhsCod").Value
                     WhsCode = oRecSetH1.Fields.Item("WhsCode").Value
                     Quantity = oRecSetH1.Fields.Item("Quantity").Value
+                    Lote = oRecSetH1.Fields.Item("ManBtchNum").Value
 
                     oStockTransfer.Lines.BaseEntry = DocEntry
                     oStockTransfer.Lines.BaseType = 5
@@ -69,7 +71,9 @@
                     oStockTransfer.Lines.WarehouseCode = WhsCode
                     oStockTransfer.Lines.Quantity = Quantity
 
-                    stQueryH2 = "Select T0.*,T1.""CreateDate"" from
+                    If Lote = "Y" Then
+
+                        stQueryH2 = "Select T0.*,T1.""CreateDate"" from
                                                     (Select ""BatchNum"",""ItemCode"",""WhsCode"",
                                                     sum(case when ""Direction""=0 then ""Quantity"" else -1*""Quantity"" end) as ""CantidadLote"" 
                                                     from IBT1 where ""ItemCode""='" & ItemCode & "' AND ""WhsCode""='" & FromWhsCod & "'
@@ -77,48 +81,50 @@
                                                     Inner Join OBTN T1 on T1.""DistNumber""=T0.""BatchNum"" and T1.""ItemCode""=T0.""ItemCode""
                                                     where T0.""CantidadLote"">0
                                                     order by T1.""CreateDate"""
-                    oRecSetH2.DoQuery(stQueryH2)
+                        oRecSetH2.DoQuery(stQueryH2)
 
-                    If oRecSetH2.RecordCount > 0 Then
+                        If oRecSetH2.RecordCount > 0 Then
 
-                        oRecSetH2.MoveFirst()
-                        CantidadR = Quantity
+                            oRecSetH2.MoveFirst()
+                            CantidadR = Quantity
 
-                        For l = 0 To oRecSetH2.RecordCount - 1
+                            For l = 0 To oRecSetH2.RecordCount - 1
 
-                            CantidadL = oRecSetH2.Fields.Item("CantidadLote").Value
+                                CantidadL = oRecSetH2.Fields.Item("CantidadLote").Value
 
-                            If CantidadR > CantidadL Then
+                                If CantidadR > CantidadL Then
 
-                                CantidadR = CantidadR - CantidadL
+                                    CantidadR = CantidadR - CantidadL
 
-                                BatchNumber = oRecSetH2.Fields.Item("BatchNum").Value
+                                    BatchNumber = oRecSetH2.Fields.Item("BatchNum").Value
 
-                                oStockTransfer.Lines.BatchNumbers.BatchNumber = BatchNumber
-                                oStockTransfer.Lines.BatchNumbers.Quantity = CantidadL
-                                oStockTransfer.Lines.BatchNumbers.BaseLineNumber = LineNum
+                                    oStockTransfer.Lines.BatchNumbers.BatchNumber = BatchNumber
+                                    oStockTransfer.Lines.BatchNumbers.Quantity = CantidadL
+                                    oStockTransfer.Lines.BatchNumbers.BaseLineNumber = LineNum
 
-                                oStockTransfer.Lines.BatchNumbers.Add()
+                                    oStockTransfer.Lines.BatchNumbers.Add()
 
-                                l = 0
+                                    l = 0
 
-                            Else
+                                Else
 
-                                BatchNumber = oRecSetH2.Fields.Item("BatchNum").Value
+                                    BatchNumber = oRecSetH2.Fields.Item("BatchNum").Value
 
-                                oStockTransfer.Lines.BatchNumbers.BatchNumber = BatchNumber
-                                oStockTransfer.Lines.BatchNumbers.Quantity = CantidadR
-                                oStockTransfer.Lines.BatchNumbers.BaseLineNumber = LineNum
+                                    oStockTransfer.Lines.BatchNumbers.BatchNumber = BatchNumber
+                                    oStockTransfer.Lines.BatchNumbers.Quantity = CantidadR
+                                    oStockTransfer.Lines.BatchNumbers.BaseLineNumber = LineNum
 
-                                oStockTransfer.Lines.BatchNumbers.Add()
+                                    oStockTransfer.Lines.BatchNumbers.Add()
 
-                                l = oRecSetH2.RecordCount - 1
+                                    l = oRecSetH2.RecordCount - 1
 
-                            End If
+                                End If
 
-                            oRecSetH2.MoveNext()
+                                oRecSetH2.MoveNext()
 
-                        Next
+                            Next
+
+                        End If
 
                     End If
 
